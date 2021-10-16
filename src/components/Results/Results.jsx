02996fb0bfebd5c2cfba1ca80cd83a67eab301cc
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-10-16 13:44:25 -0400

    Removed dependency in useEffect to stop rendering errors and created a function for skin type under Results.jsx

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -5,9 +5,34 @@ const Results = ({products, userPreferences}) => {
     const [filteredProducts, setFilteredProducts] = useState([])
 
     useEffect(()=>{
+        // let skinType;
+        // if(userPreferences.skinType=== "Normal"){
+        //     skinType = products.filter((product)=> product.category==="Moisturizer")
+            
+        // } else if (userPreferences.skinType=== "Combination"){
+        //     skinType = products.filter((product)=> product.category==="Serum")
 
-    })
+        // } else {
+        //     skinType = products.filter((product)=> product.category==="Exfoliant")
 
+        // }
+        // setFilteredProducts(skinType)
+        skinType();
+    },[])
+
+    async function skinType(){
+        if(userPreferences.skinType=== "Normal"){
+            skinType = products.filter((product)=> product.category==="Moisturizer")
+            
+        } else if (userPreferences.skinType=== "Combination"){
+            skinType = products.filter((product)=> product.category==="Serum")
+
+        } else {
+            skinType = products.filter((product)=> product.category==="Exfoliant")
+
+        }
+        setFilteredProducts(skinType)
+    }
     return (
         <div>
           {filteredProducts.map((product)=>(
