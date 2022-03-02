4266f5c6742f0f302e9e7a4c8ebef5ce9f7e63f0
charmaine llacuna <charmaine.llacuna@yahoo.com>
2022-03-01 23:40:45 -0500

    Added a photo to the login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -30,42 +30,39 @@ class Login extends Component {
 
   render() {
     return (
-
-    <section className="sign-in">
-        <div className="container">
-            <div className="signin-content">
-
-                <div className="signin-form">
-                    <h2 className="form-title">Sign in</h2>
-                    <form onSubmit={this.handleSubmit} className="login-form" id="login-form">
-                        <div className="form-group">
-                            <input type="text" name="username"  onChange={this.handleChange} value={this.state.username} placeholder="Your Username"/>
+      <div class="container">
+          <div class="row">
+            <div class="col">
+              <div className="container">
+                        <div className="signin-form">
+                            <h2 className="form-title">Sign in</h2>
+                            <form onSubmit={this.handleSubmit} className="login-form" id="login-form">
+                                <div className="form-group">
+                                    <input type="text" name="username"  onChange={this.handleChange} value={this.state.username} placeholder="Your Username"/>
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password"/>
+                                </div>
+                                <div className="form-group">
+                                </div>
+                                <div className="form-group form-button">
+                                    <input type="submit"  className="form-submit" value="Log in"/>
+                                </div>
+                            </form>
+                            <div className="social-login">
+                                <span className="social-label">Don't have an account?</span>
+                                <ul className="socials">
+                                    <li><a href="/register">Register Here</a></li>
+                                </ul>
+                            </div>
+                          </div>
                         </div>
-                        <div className="form-group">
-                            <input type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password"/>
-                        </div>
-                        <div className="form-group">
-                            
-                        </div>
-                        <div className="form-group form-button">
-                            <input type="submit"  className="form-submit" value="Log in"/>
-                        </div>
-                    </form>
-                    <div className="social-login">
-                        <span className="social-label">Don't have an account?</span>
-                        <ul className="socials">
-                            <li><a href="/register">Register Here</a></li>
-                            
-                        </ul>
-                    </div>
-                    
-                </div>
+                       </div>
+              <div class="col">
+                <img src="/images/unsplash.jpg" className="image" />  
+              </div>
             </div>
         </div>
-    </section>
-
-   
-
     );
   }
 }
