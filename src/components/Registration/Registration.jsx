0661fc75f90247701566b7101ad9694451310fd7
charmaine llacuna <charmaine.llacuna@yahoo.com>
2022-03-02 20:06:55 -0500

    Fixed image size and removed width property under register-form in registration.css

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -26,19 +26,19 @@ const Registration = ({ registerUser }) => {
                     <div className="signup-form">
                         <h2 className="form-title">Sign up</h2>
                         <form onSubmit={handleSubmit} className="register-form" id="register-form">
-                            <div className="form-group">
-                                <input type="text" value = { firstName } onChange={(event) => setFirstName(event.target.value)} placeholder="Your First Name"/>
+                            <div className="form-group-r">
+                                <input type="text" value = { firstName } onChange={(event) => setFirstName(event.target.value)} placeholder="First Name"/>
                             </div>
-                            <div className="form-group">
-                                <input type="lastName" value = { lastName } onChange={(event) => setLastName(event.target.value)} placeholder="Your Last Name"/>
+                            <div className="form-group-r">
+                                <input type="lastName" value = { lastName } onChange={(event) => setLastName(event.target.value)} placeholder="Last Name"/>
                             </div>
-                             <div className="form-group">
+                             <div className="form-group-r">
                                 <input type="username" value = { username } onChange={(event) => setUsername(event.target.value)} placeholder="Username"/>
                             </div>
-                            <div className="form-group">
+                            <div className="form-group-r">
                                 <input type="password" value = { password } onChange={(event) => setPassword(event.target.value)} placeholder="Password"/>
                             </div>
-                             <div className="form-group">
+                             <div className="form-group-r">
                                 <input type="email" value = { email } onChange={(event) => setEmail(event.target.value)} placeholder="Email"/>
                             </div>
                             
@@ -53,7 +53,7 @@ const Registration = ({ registerUser }) => {
                 </div>
             </div>
              <div class="col">
-                <img src="/images/soap.jpg" className="image" />  
+                <img src="/images/soap.jpg" className="registration-image" />  
             </div>
         </div>
     </div>
