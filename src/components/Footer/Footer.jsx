148d8241869aa697156a3fb9547b62067bea7363
charmaine llacuna <charmaine.llacuna@yahoo.com>
2022-05-22 23:04:13 -0400

    Included links to social media icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ function Footer(){
     return(
         <div className="footer-links">
             <p>Thanks for visiting 💛</p>
-            <p>Get in touch <ion-icon name="phone-portrait-outline"></ion-icon></p>
-            <ion-icon name="logo-linkedin"></ion-icon>
-            <ion-icon name="logo-github"></ion-icon>
-            <ion-icon name="mail-outline"></ion-icon>
+            <p>Get in touch </p>
+            <a href="https://www.linkedin.com/in/charmaine-llacuna"><ion-icon name="logo-linkedin"></ion-icon></a>
+            <a href="https://github.com/Char-Alexis"><ion-icon name="logo-github"></ion-icon></a>
+            <a href="mailto:charmaine.llacuna@yahoo.com"><ion-icon name="mail-outline"></ion-icon></a>
         </div>
     )
     
