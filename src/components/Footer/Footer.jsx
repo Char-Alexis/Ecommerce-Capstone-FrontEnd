71dd6ba8c7d1b2b78615547e2c1e831d78f18eea
charmaine llacuna <charmaine.llacuna@yahoo.com>
2022-05-23 22:55:59 -0400

    Added margins and padding to footer elements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,21 @@ import "./Footer.css";
 function Footer(){
     return(
         <div className="footer-links">
-            <p>Thanks for visiting 💛</p>
-            <p>Get in touch </p>
-            <a href="https://www.linkedin.com/in/charmaine-llacuna"><ion-icon name="logo-linkedin"></ion-icon></a>
-            <a href="https://github.com/Char-Alexis"><ion-icon name="logo-github"></ion-icon></a>
-            <a href="mailto:charmaine.llacuna@yahoo.com"><ion-icon name="mail-outline"></ion-icon></a>
+            
+            <div className="font">Thanks for visiting! 💛</div>
+            <p>Get in touch via email at ㅤ 
+                <span>
+                    <a className="email" href="mailto:charmaine.llacuna@yahoo.com"> charmaine.llacuna@yahoo.com </a> 
+                
+                </span> 
+                or find me at
+                </p>
+            <a className="linkElement" href="https://www.linkedin.com/in/charmaine-llacuna"><ion-icon name="logo-linkedin"></ion-icon></a>
+            <a className="linkElement" href="https://github.com/Char-Alexis"><ion-icon name="logo-github"></ion-icon></a>
+            <a className="linkElement" href="mailto:charmaine.llacuna@yahoo.com"><ion-icon name="mail-outline"></ion-icon></a>
         </div>
     )
+    
     
 };
 
